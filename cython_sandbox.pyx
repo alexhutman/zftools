4be5ac9992a6a2d30f4e4ec946b542f3d7880e91
Louis Deaett <deaett@Louiss-MacBook-Air.local>
2018-05-16 12:12:36 -0400

    Added more Sage+Cython code.

--- cython_sandbox.pyx
+++ cython_sandbox.pyx
@@ -2,6 +2,10 @@
 This code is just an attempt to do something in Sage with graphs that can run compiled using Cython and the appropriate libraries.
 """
 
+from sage.graphs.all import Graph 
+include "sage/data_structures/bitset.pxi"
+           
+
 
 def compute_from_graph(graph):
 	"""
@@ -24,10 +28,28 @@ def compute_from_graph(graph):
 	
 	print "This is a graph!"
 	
-	# eventually call something like the below
+	num_vertices = graph.num_verts()
+	
 	# create pointer to bitset array with neighborhoods
-	# call cython code and pass it number of vertices and
-	# list of their neighborhoods
-	# result = do_compiled_stuff_with_graph(n, neighborhood_bitsets)
+	cdef bitset_s *neighborhood_array = <bitset_s *> sage_malloc(num_vertices*sizeof(bitset_s))
+	for v in range(num_vertices):
+		bitset_init(&neighborhood_array[v], num_vertices)
+		bitset_clear(&neighborhood_array[v])
+		for w in graph.neighbors(v):
+			bitset_add(&neighborhood_array[v], w)
+
+	result = do_compiled_stuff_with_graph(num_vertices, neighborhood_array)
 	# return result
+
+
+cdef do_compiled_stuff_with_graph(int n, bitset_s *neighborhood_bitsets):
+	cdef char* s = NULL
+
+	print "Graph has", n, "vertices!"
+
+	print "Here are the neighborhoods as bitsets:"
+	for v in range(n):
+		print bitset_chars(s, &neighborhood_bitsets[v])
+#		print j
+	return
 	
