9fddd49e783dab2b67d235ad645f15769eca38ca
alexhutman <alexhutman@gmail.com>
2018-10-11 16:35:39 -0400

    Removed class, all 1 pyx file now
    Not currently working!

--- all_closures.py
+++
@@ -1,9 +0,0 @@
-import ZFSearchMetagraphNewAlgPostClose
-
-def calculate_all_closures(metagraph):
-    ourGraph = metagraph.primal_graph
-    
-    print "vertices :", ourGraph.vertices()
-    
-    for S in Subsets(ourGraph.vertices()):
-        metagraph.extend_closure(S,Set([]))
