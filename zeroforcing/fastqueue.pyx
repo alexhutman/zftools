19f695b628c7f5279e5126eba7b77c6edc05ecf6
Alex <alexhutman@gmail.com>
2023-01-28 01:06:50 -0500

    Made FastQueue insanely blazingly fast
    Used a heapqueue to manage the rank of priorities and a dict to manage
    the items themselves. Must issue a warning with this commit because the
    improvements seem way too good to be true...

--- zeroforcing/fastqueue.pyx
+++ zeroforcing/fastqueue.pyx
@@ -1,14 +1,11 @@
+import heapq
+
 # Disabling bounds checking in this class sounds like it would be perfect here(?)
 cdef class FastQueueForBFS:
     def __init__(self, max_priority):
-        self.max_possible_priority = max_priority
-        self.smallest_nonempty_priority = UINT_MAX
-        
         self.length = 0
-        self.array_list = list()
-
-        for i in range(max_priority+1):
-            self.array_list.append(list())
+        self.heapqueue = list()
+        self.heapqueue_elements = {i: list() for i in range(max_priority+1)}
 
     def __len__(self):
         return self.length
@@ -18,10 +15,10 @@ cdef class FastQueueForBFS:
         # raise ValueError if priority_for_new_item > self.max_possible_priority?
             # Not checking makes it faster though :)
 
-        self.array_list[priority_for_new_item].append(new_item)
+        if len(self.heapqueue_elements[priority_for_new_item]) == 0:
+            heapq.heappush(self.heapqueue, priority_for_new_item)
+        self.heapqueue_elements[priority_for_new_item].append(new_item)
         self.length += 1
-        
-        self.smallest_nonempty_priority = min(priority_for_new_item, self.smallest_nonempty_priority)
 
     cdef object pop(self):
         cdef unsigned int _
@@ -32,12 +29,8 @@ cdef class FastQueueForBFS:
 
     cdef tuple pop_and_get_priority(self):
         # Store vals to return
-        cdef unsigned int priority_to_return = self.smallest_nonempty_priority
-        cdef object item_to_return = self.array_list[priority_to_return].pop()
+        cdef unsigned int priority_to_return = heapq.heappop(self.heapqueue)
+        cdef object item_to_return = self.heapqueue_elements[priority_to_return].pop()
         self.length -= 1
 
-        # Find new smallest priority
-        while len(self.array_list[self.smallest_nonempty_priority]) == 0 \
-        and self.smallest_nonempty_priority < self.max_possible_priority:
-            self.smallest_nonempty_priority += 1
         return priority_to_return, item_to_return
