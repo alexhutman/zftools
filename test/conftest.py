c6fefdf16f82f15a56d7ae29e9b6847f129aa95a
Alex <alexhutman@gmail.com>
2023-02-05 23:41:54 -0500

    Formatted all .py files
    I used Black but it doesn't look like it supports Cython files yet.

--- test/conftest.py
+++ test/conftest.py
@@ -12,6 +12,7 @@ _SHOULD_PROFILE_FLAG = "--profile"
 _PROFILER_ENABLED_TAG = "profiler_enabled"
 _PROFILER_RESULTS_SECTION = "profiler results:"
 
+
 def check_positive(value):
     err = argparse.ArgumentTypeError("Provide a positive integer.")
     try:
@@ -23,37 +24,44 @@ def check_positive(value):
 
     raise err
 
+
 def help_str_with_default(help_str, default):
     return f"{help_str} (default: {default})"
+
 
 def pytest_addoption(parser):
     profile_group = parser.getgroup("Zero forcing options")
     profile_group.addoption(
-            _SHOULD_PROFILE_FLAG,
-            action="store_true",
-            help=help_str_with_default("Whether to profile the tests or not. Not too helpful unless you compile in debug mode.", False)
-            )
+        _SHOULD_PROFILE_FLAG,
+        action="store_true",
+        help=help_str_with_default(
+            "Whether to profile the tests or not. Not too helpful unless you compile in debug mode.", False
+        ),
+    )
     # Pytest uses their own version of options, don't think they support subparsers
     profile_group.addoption(
-            _NUM_RESULTS_FLAG,
-            type=check_positive,
-            help=help_str_with_default("Number of profiler results to return.", _DEFAULT_NUM_RESULTS),
-            default=_DEFAULT_NUM_RESULTS
-            )
+        _NUM_RESULTS_FLAG,
+        type=check_positive,
+        help=help_str_with_default("Number of profiler results to return.", _DEFAULT_NUM_RESULTS),
+        default=_DEFAULT_NUM_RESULTS,
+    )
+
 
 def pytest_sessionstart(session):
     session.results = dict()
+
 
 def pytest_sessionfinish(session, exitstatus):
     passed_amount = sum(1 for result in session.results.values() if result.passed)
     failed_amount = sum(1 for result in session.results.values() if result.failed)
 
+
 @pytest.hookimpl(tryfirst=True, hookwrapper=True)
-def pytest_runtest_makereport(item, call): # https://stackoverflow.com/a/51714659
+def pytest_runtest_makereport(item, call):  # https://stackoverflow.com/a/51714659
     outcome = yield
     result = outcome.get_result()
 
-    if result.when == 'call':
+    if result.when == "call":
         item.session.results[item] = result
 
     # On teardown of last item
@@ -62,9 +70,11 @@ def pytest_runtest_makereport(item, call): # https://stackoverflow.com/a/5171465
         section = (_PROFILER_RESULTS_SECTION, profiler_results)
         result.sections.append(section)
 
+
 def pytest_report_header(config, start_path, startdir):
     should_profile = config.getoption(_SHOULD_PROFILE_FLAG)
     print(f"{_PROFILER_ENABLED_TAG}:", should_profile)
+
 
 def pytest_collection_modifyitems(session, config, items):
     should_profile = config.getoption(_SHOULD_PROFILE_FLAG)
@@ -76,13 +86,15 @@ def pytest_collection_modifyitems(session, config, items):
         # Arguably should do this in the sessionfinish hook but..
         items[-1].should_display_profile_results = True
 
+
 def pytest_terminal_summary(terminalreporter, exitstatus, config):
-    reports = terminalreporter.getreports('')
+    reports = terminalreporter.getreports("")
     content = os.linesep.join(text for report in reports for secname, text in report.sections)
     if content:
         terminalreporter.ensure_newline()
         terminalreporter.section(_PROFILER_RESULTS_SECTION, blue=True, bold=True)
         terminalreporter.write_line(content + os.linesep)
+
 
 @pytest.fixture(scope="session")
 def profiler(request):
