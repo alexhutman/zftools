f7d3ee0f7455e5d807c9d7d917075f853dfcf0cc
Alex <alexhutman@gmail.com>
2024-05-24 11:24:50 -0400

    [fastqueue.p{yx,xd}] Use `size_t` instead of `unsigned int`

--- zeroforcing/fastqueue.pxd
+++ zeroforcing/fastqueue.pxd
@@ -1,12 +1,9 @@
-cdef extern from "limits.h":
-    cdef unsigned int UINT_MAX
-
 cdef class FastQueueForBFS:
     cdef:
         list array_list
-        unsigned int max_possible_priority
-        unsigned int smallest_nonempty_priority
+        size_t max_possible_priority
+        size_t smallest_nonempty_priority
 
-        void push(self, unsigned int, object)
+        void push(self, size_t, object)
         object pop(self)
         tuple pop_and_get_priority(self)
--- zeroforcing/fastqueue.pyx
+++ zeroforcing/fastqueue.pyx
@@ -1,12 +1,15 @@
+cdef extern from "stdint.h":
+    cdef size_t SIZE_MAX
+
 # Disabling bounds checking in this class sounds like it would be perfect here(?)
 cdef class FastQueueForBFS:
     def __init__(self, max_priority):
         self.max_possible_priority = max_priority
-        self.smallest_nonempty_priority = UINT_MAX
+        self.smallest_nonempty_priority = SIZE_MAX
         
         self.array_list = [list() for _ in range(max_priority+1)]
 
-    cdef void push(self, unsigned int priority_for_new_item, object new_item):
+    cdef void push(self, size_t priority_for_new_item, object new_item):
         # Check for negative here?
         # raise ValueError if priority_for_new_item > self.max_possible_priority?
             # Not checking makes it faster though :)
@@ -16,7 +19,7 @@ cdef class FastQueueForBFS:
         self.smallest_nonempty_priority = min(priority_for_new_item, self.smallest_nonempty_priority)
 
     cdef object pop(self):
-        cdef unsigned int _
+        cdef size_t _
         cdef object popped
 
         _, popped = self.pop_and_get_priority()
@@ -24,7 +27,7 @@ cdef class FastQueueForBFS:
 
     cdef tuple pop_and_get_priority(self):
         # Store vals to return
-        cdef unsigned int priority_to_return = self.smallest_nonempty_priority
+        cdef size_t priority_to_return = self.smallest_nonempty_priority
         cdef object item_to_return = self.array_list[priority_to_return].pop()
 
         # Find new smallest priority
