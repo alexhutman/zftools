50690ea2b86637fd2078e4541ff3e02272196ae6
Alex <alexhutman@gmail.com>
2024-07-21 13:38:53 -0400

    Almost there
    Got everything to copy over and start building correctly, now we're just running into this issue:
    #8 5.121       building 'zeroforcing.metagraph' extension                                                                                                                                   #8 5.121       gcc -Wsign-compare -DNDEBUG -g -fwrapv -O3 -Wall -Wno-unused -g -O2 -fPIC -I/home/sage/sage/local/var/lib/sage/venv-python3.11.1/include/python3.11 -c zeroforcing/metagraph.c -o build/temp.linux-x86_64-cpython-311/zeroforcing/metagraph.o
    #8 5.121       zeroforcing/metagraph.c:1219:10: fatal error: struct_signals.h: No such file or directory
    #8 5.121        1219 | #include "struct_signals.h"
    #8 5.121             |          ^~~~~~~~~~~~~~~~~~
    
    I believe I ran into this before, but can't remember how I fixed it...

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup, find_packages, Extension
 from setuptools.command.build_ext import build_ext as build_ext_orig
 from setuptools.command.build import build as build_orig
 
+import sys
 #from Cython.Build import cythonize
 #from Cython.Build import build_ext
 
@@ -12,12 +13,6 @@ _extension_modules = [
     # TODO: Add flag whether or not to compile this
     Extension("test.verifiability.wavefront", sources=["test/verifiability/wavefront.pyx"]),
 ]
-
-#class InstallZeroForcing(build_ext):
-    # Normal build_ext + forces the "--inplace" flag
-    #def initialize_options(self, *args, **kwargs):
-        #super().initialize_options(*args, **kwargs)
-        #self.inplace = True
 
 # https://stackoverflow.com/a/26698408/6708303
 class lazy_cythonize(list):
@@ -31,40 +26,30 @@ class lazy_cythonize(list):
     def __getitem__(self, ii): return self.c_list()[ii]
     def __len__(self): return len(self.c_list())
 
-class build(build_orig):
-    def finalize_options(self):
-        super().finalize_options()
-        from Cython.Build import cythonize
-        print("HELLO FROM SETUP.PY")
-        self.distribution.ext_modules = cythonize(self.distribution.ext_modules,
-                                                  language_level=3)
-#class build_ext(build_ext_orig):
+#class build(build_orig):
     #def finalize_options(self):
         #super().finalize_options()
         #from Cython.Build import cythonize
         #print("HELLO FROM SETUP.PY")
         #self.distribution.ext_modules = cythonize(self.distribution.ext_modules,
                                                   #language_level=3)
+class build_ext(build_ext_orig):
+    #def initialize_options(self, *args, **kwargs):
+        #super().initialize_options(*args, **kwargs)
+        #self.inplace = True
 
-def _get_setup_parameters(extensions):
-    setup_params = {
-        "name": "zeroforcing",
-        #"packages": find_packages(exclude=('test*')),
-        #"package_data": {ext.name: ext.sources for ext in extensions},
-        #"cmdclass": {"build_ext": InstallZeroForcing}
-        #"cmdclass": {"build_ext": build_ext}
-        "cmdclass": {"build": build}
-    }
-    print(setup_params)
-    #exit(1)
-    return setup_params
+    def run(self):
+        print("HELLO FROM SETUP.PY")
+        print(f"Before Cythonizing {self.distribution.ext_modules=}")
+        self.distribution.ext_modules = cythonize_extensions(self.distribution.ext_modules)
+        print(f"After Cythonizing {self.distribution.ext_modules=}")
+        super().run()
 
-
-
-    is_debug = False
+def cythonize_extensions(extensions, debug=False):
+    from Cython.Build import cythonize
 
     force_release_ext_names = {"test.verifiability.wavefront"}
-    debug_exts = [ext for ext in extensions if is_debug and ext.name not in force_release_ext_names]
+    debug_exts = [ext for ext in extensions if debug and ext.name not in force_release_ext_names]
     release_exts = [ext for ext in extensions if ext.name not in {e.name for e in debug_exts}]
     comp_directives = {"language_level": '3', "binding": False}
 
@@ -79,10 +64,33 @@ def _get_setup_parameters(extensions):
         print(f"Compiling the following extensions in release mode: {', '.join((ext.name for ext in release_exts))}")
         cythonized.extend(cythonize(release_exts, compiler_directives=comp_directives))
 
-    setup_params.update({"ext_modules": cythonized})
+    #setup_params.update({"ext_modules": cythonized})
+    return cythonized
+
+def _get_setup_parameters(extensions):
+    is_building_bdist_wheel = "bdist_wheel" in sys.argv
+    setup_params = {
+        "name": "zeroforcing",
+        "version": "0.1",
+        "packages": find_packages(exclude=('test*')),
+        #"package_data": {ext.name: ext.sources for ext in extensions},
+        #"cmdclass": {"build_ext": InstallZeroForcing}
+        "ext_modules": _extension_modules
+        #"cmdclass": {"build": build}
+    }
+
+    # https://stackoverflow.com/a/41629156/6708303
+    if is_building_bdist_wheel:
+        setup_params.update({"cmdclass": {"build_ext": build_ext}})
+    print(setup_params)
+    #exit(1)
     return setup_params
 
+
+
+
 def main():
+    print(f"{sys.argv=}")
     setup(**_get_setup_parameters(_extension_modules))
 
 
