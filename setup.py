e8fb5e5d1cf67dcb5bf250057c5ef5bae005f037
Alex <alexhutman@gmail.com>
2024-07-22 14:38:08 -0400

    Removed `build_ext` override
    We can revisit all of the fancy stuff later... Most important part is pip for now.

--- setup.py
+++ setup.py
@@ -7,43 +7,6 @@ class no_egg(_bdist_egg):
         from distutils.errors import DistutilsOptionError
         raise DistutilsOptionError("Honestly just copying https://github.com/sagemath/cysignals/blob/c901dc9217de735c67ca5daf3dff6276813a05b5/setup.py#L186-L193")
 
-"""
-class build_ext(build_ext_orig):
-    #def initialize_options(self, *args, **kwargs):
-        #super().initialize_options(*args, **kwargs)
-        #self.inplace = True
-
-    def cythonize_extensions(self, debug=False):
-        from Cython.Build.Dependencies import cythonize
-        #from Cython.Build import cythonize
-
-        force_release_ext_names = {"test.verifiability.wavefront"}
-        debug_exts = [ext for ext in self.distribution.ext_modules if debug and ext.name not in force_release_ext_names]
-        release_exts = [ext for ext in self.distribution.ext_modules if ext.name not in {e.name for e in debug_exts}]
-        comp_directives = {"language_level": '3', "binding": False}
-
-        cythonized = []
-        if debug_exts:
-            for ext in debug_exts:
-                ext.define_macros = [("CYTHON_TRACE", 1)]
-            print(f"Compiling the following extensions in debug mode: {', '.join((ext.name for ext in debug_exts))}")
-            cythonized.extend(cythonize(debug_exts, build_dir="build", compiler_directives={**comp_directives, "linetrace": True}))
-
-        if release_exts:
-            print(f"Compiling the following extensions in release mode: {', '.join((ext.name for ext in release_exts))}")
-            cythonized.extend(cythonize(release_exts, build_dir="build", compiler_directives=comp_directives))
-
-        self.distribution.ext_modules = cythonized
-
-    def run(self):
-        print("HELLO FROM SETUP.PY")
-
-        print(f"Before Cythonizing {self.distribution.ext_modules=}")
-        self.cythonize_extensions()
-        print(f"After Cythonizing {self.distribution.ext_modules=}")
-        super().run()
-"""
-
 
 def get_setup_parameters(extensions):
     setup_params = {
