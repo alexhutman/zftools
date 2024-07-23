3b32b25a366637216f262b495b97386613b84043
Alex <alexhutman@gmail.com>
2024-07-23 11:28:44 -0400

    [setup.py] Added custom Cythonizer
    Thanks Cysignals, pls don't sue k thx
    https://github.com/sagemath/cysignals/blob/c901dc9217de735c67ca5daf3dff6276813a05b5/setup.py#L163-L176

--- setup.py
+++ setup.py
@@ -17,7 +17,30 @@ class no_egg(_bdist_egg):
         from distutils.errors import DistutilsOptionError
         raise DistutilsOptionError("Honestly just copying https://github.com/sagemath/cysignals/blob/c901dc9217de735c67ca5daf3dff6276813a05b5/setup.py#L186-L193")
 
-class build_wavefront(_build_ext):
+class zf_cythonize(_build_ext):
+    base_directives = dict(
+             binding=False,
+             language_level=3,
+            )
+    def run(self):
+        dist = self.distribution
+        ext_modules = dist.ext_modules
+        if ext_modules:
+            dist.ext_modules[:] = self.cythonize(ext_modules)
+        super().run()
+
+    def cythonize(self, extensions):
+        # Run Cython with -Werror on continuous integration services
+        # with Python 3.6 or later
+        from Cython.Compiler import Options
+        Options.warning_errors = False
+
+        compiler_directives = dict(**self.base_directives)
+        from Cython.Build.Dependencies import cythonize
+        return cythonize(extensions,
+                         compiler_directives=compiler_directives)
+
+class build_wavefront(zf_cythonize):
     def initialize_options(self):
         super().initialize_options()
         ext_name = "zeroforcing.test.verifiability.wavefront"
@@ -42,7 +65,7 @@ def get_setup_parameters(extensions):
         "extras_require": { "test": ['pytest'] }
     }
 
-    cmdclass = {"bdist_egg": no_egg, "build_ext": _build_ext}
+    cmdclass = {"bdist_egg": no_egg, "build_ext": zf_cythonize}
     if SAGE_INSTALLED:
         cmdclass.update({"build_cython": sage_build_cython, "wavefront": build_wavefront})
 
