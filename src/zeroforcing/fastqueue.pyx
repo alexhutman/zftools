5d7b673841bcd0aa236c463f9c37a537ef7db7ad
Alex <alexhutman@gmail.com>
2024-07-30 14:51:46 -0400

    Linted .pyx and .pxd files

--- src/zeroforcing/fastqueue.pyx
+++ src/zeroforcing/fastqueue.pyx
@@ -6,17 +6,19 @@ cdef class FastQueueForBFS:
     def __init__(self, max_priority):
         self.max_possible_priority = max_priority
         self.smallest_nonempty_priority = SIZE_MAX
-        
+
         self.array_list = [list() for _ in range(max_priority+1)]
 
     cdef void push(self, size_t priority_for_new_item, object new_item):
         # Check for negative here?
         # raise ValueError if priority_for_new_item > self.max_possible_priority?
-            # Not checking makes it faster though :)
+        # Not checking makes it faster though :)
 
         self.array_list[priority_for_new_item].append(new_item)
-        
-        self.smallest_nonempty_priority = min(priority_for_new_item, self.smallest_nonempty_priority)
+
+        self.smallest_nonempty_priority = min(
+                priority_for_new_item,
+                self.smallest_nonempty_priority)
 
     cdef object pop(self):
         cdef size_t _
@@ -31,7 +33,7 @@ cdef class FastQueueForBFS:
         cdef object item_to_return = self.array_list[priority_to_return].pop()
 
         # Find new smallest priority
-        while len(self.array_list[self.smallest_nonempty_priority]) == 0 \
-        and self.smallest_nonempty_priority < self.max_possible_priority:
+        while (len(self.array_list[self.smallest_nonempty_priority]) == 0 and
+               self.smallest_nonempty_priority < self.max_possible_priority):
             self.smallest_nonempty_priority += 1
         return priority_to_return, item_to_return
